cccd152e8b4e84e24b21997771d267e34c8ace79
somecoolusername <jacobcampbell111@outlook.com>
2017-10-24 13:43:17 +0800

    Added unit tests

---
+++ UnitTest/JsonHandlerUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonHandler;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class JsonHandlerUnitTest
+    {
+        static string testData =
+                    "\"Black\"";
+
+        [TestMethod]
+        public void TestMethod1_ToSerializeJsonObject()
+        {
+            Color black = Color.Black;
+
+            string data = JsonHandler.JsonHandler.ToSerializeJsonObject(black);
+            Assert.AreEqual(data, testData);
+        }
+
+        [TestMethod]
+        public void TestMethod1_ToDeserializeJsonObject()
+        {
+            Color black = Color.Black;
+
+            Color test = JsonHandler.JsonHandler.ToDeserializeJsonObject<Color>(testData);
+            Assert.AreEqual(test, black);
+        }
+    }
+}
---
+++ UnitTest/MyColorsUnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyColors;
+using System.Drawing;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MyColorsUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1_colorName()
+        {
+            MyColor color = new MyColor(Color.Black);
+            Assert.AreEqual(Color.Black.Name, color.Name);
+        }
+
+        [TestMethod]
+        public void TestMethod1_colorHex()
+        {
+            MyColor color = new MyColor(Color.Black);
+            Assert.AreEqual(Color.Black.ToArgb().ToString("X6"), color.HtmlHex);
+        }
+    }
+}
